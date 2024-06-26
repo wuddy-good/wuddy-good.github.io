4030a1ddb2b74e0a57567447284f764fcc311ff2
Vlad Polishchuk <vladpolisuk127@gmail.com>
2024-06-26 07:57:30 +0300

    build: update gitignore and next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    newNextLinkBehavior: false,
-  },
+  distDir: "dist",
 };
 
 export default nextConfig;
